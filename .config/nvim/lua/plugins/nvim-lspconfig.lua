326e95c81e968df9619bec7f532d31a9a7e54d0c
davidpetti <pettid412@gmail.com>
2024-09-23 16:52:32 -0400

    Fix Errors

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,32 +1,32 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.tsserver.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.gopls.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.rust_analyzer.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.pyright.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.clangd.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.arduino_language_server.setup({
-            capabilities = capabilities,
-        })
+	"neovim/nvim-lspconfig",
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.gopls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.rust_analyzer.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.arduino_language_server.setup({
+			capabilities = capabilities,
+		})
 
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-    end,
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+	end,
 }
