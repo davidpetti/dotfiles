bba24ed3b397e9cd1bca6dab7e0b26391d3031df
davidpetti <pettid412@gmail.com>
2025-03-27 22:59:56 -0400

    Change to Gruvbox theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
-return {
-	-- "Mofiqul/dracula.nvim",
-	"catppuccin/nvim",
-	priority = 1000,
-	config = function()
-		-- vim.cmd([[colorscheme dracula]])
-		vim.cmd([[colorscheme catppuccin]])
-	end,
-}
+-- return {
+-- 	-- "Mofiqul/dracula.nvim",
+-- 	"catppuccin/nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		-- vim.cmd([[colorscheme dracula]])
+-- 		vim.cmd([[colorscheme catppuccin]])
+-- 	end,
+-- }
+
+return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,9 +4,11 @@ return {
 	opts = {
 		options = {
 			icons_enabled = true,
-			theme = "auto",
-			component_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
+			theme = "gruvbox_dark",
+			-- component_separators = { left = "", right = "" },
+			-- section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
 			disabled_filetypes = {
 				statusline = {},
 				winbar = {},
