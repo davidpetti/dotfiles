8c03abbb3abbb9d128b834b7566670234f57619b
davidpetti <pettid412@gmail.com>
2026-01-21 12:33:27 -0500

    Update: nvim colorscheme to gruvbox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,15 @@
 -- return {
--- 	-- "Mofiqul/dracula.nvim",
 -- 	"catppuccin/nvim",
 -- 	priority = 1000,
 -- 	config = function()
--- 		-- vim.cmd([[colorscheme dracula]])
 -- 		vim.cmd([[colorscheme catppuccin]])
 -- 	end,
 -- }
 
-return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... }
+return {
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		vim.cmd([[colorscheme gruvbox]])
+	end,
+}
