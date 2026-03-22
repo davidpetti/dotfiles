59726a14db64ed5b13c13d418842c325c2715d7f
davidpetti <pettid412@gmail.com>
2026-03-22 16:45:22 -0400

    Update: remove old colorscheme comment

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,3 @@
--- return {
--- 	"catppuccin/nvim",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd([[colorscheme catppuccin]])
--- 	end,
--- }
-
 return {
 	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
