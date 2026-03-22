a79be4438573bb64a81b09e0ab257e7a6ec7d7b0
davidpetti <pettid412@gmail.com>
2026-03-22 16:49:40 -0400

    Update: formatting and keybind configs

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	keys = {
+		{ "<leader>e", "<cmd>NvimTreeToggle<cr>", mode = "n", desc = "Toggle file tree" },
+	},
 	opts = {
 		sort = {
 			sorter = "case_sensitive",
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,22 +1,40 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
-	opts = {
-		extensions = {
-			["ui-select"] = {
-				require("telescope.themes").get_dropdown({}),
-			},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			mode = "n",
+			desc = "Find files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").git_files()
+			end,
+			mode = "n",
+			desc = "Find git files",
+		},
+		{
+			"<leader>fs",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			mode = "n",
+			desc = "Grep string",
 		},
 	},
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-
-		require("telescope").load_extension("ui-select")
+	opts = {
+		extensions = {
+			["ui-select"] = require("telescope.themes").get_dropdown({}),
+		},
+	},
+	config = function(_, opts)
+		local telescope = require("telescope")
+		telescope.setup(opts)
+		telescope.load_extension("ui-select")
 	end,
 }
