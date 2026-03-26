5f831fed6a2d5f70b3162fe985e88cf249c6ac91
davidpetti <pettid412@gmail.com>
2026-03-25 23:21:20 -0400

    Change: flash colorscheme

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -10,4 +10,19 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
+  config = function(_, opts)
+    require("flash").setup(opts)
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = function()
+        vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#282828", bg = "#fb4934", bold = true })
+        -- vim.api.nvim_set_hl(0, "FlashMatch", { fg = "#fabd2f", bold = true })
+        -- vim.api.nvim_set_hl(0, "FlashCurrent", { fg = "#fb4934", bold = true })
+        -- vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#7c6f64" })
+      end,
+    })
+
+    -- apply immediately too (for startup)
+    vim.cmd("doautocmd ColorScheme")
+  end,
 }
