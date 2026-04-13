1abb0d194eb4acf0bb8283f9bee636926a1935b0
davidpetti <pettid412@gmail.com>
2026-04-13 18:39:30 -0400

    Add: Debugger config and binds

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -1,7 +1,70 @@
 return {
 	"mfussenegger/nvim-dap",
 	keys = {
-		{ "<leader>db", ":DapToggleBreakpoint<CR>", mode = "n", desc = "DAP toggle breakpoint" },
-		{ "<leader>dr", ":DapContinue<CR>", mode = "n", desc = "DAP continue" },
+		{
+			"<leader>db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "DAP Breakpoint",
+		},
+		{
+			"<leader>dB",
+			function()
+				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end,
+			desc = "DAP Conditional breakpoint",
+		},
+
+		{
+			"<leader>dc",
+			function()
+				require("dap").continue()
+			end,
+			desc = "DAP Continue",
+		},
+		{
+			"<leader>di",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "DAP Step into",
+		},
+		{
+			"<leader>do",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "DAP Step over",
+		},
+		{
+			"<leader>dO",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "DAP Step out",
+		},
+
+		{
+			"<leader>dr",
+			function()
+				require("dap").repl.open()
+			end,
+			desc = "DAP Open REPL",
+		},
+		{
+			"<leader>dl",
+			function()
+				require("dap").run_last()
+			end,
+			desc = "DAP Run last",
+		},
+		{
+			"<leader>dt",
+			function()
+				require("dap").terminate()
+			end,
+			desc = "DAP Terminate",
+		},
 	},
 }
